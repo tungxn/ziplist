b14615e71535b7158b26f1eed120dd818284cb67
tungxn <tungxn@hawaii.edu>
2023-02-06 22:17:40 -1000

    E27: Five problems every software engineer should be able to solve (Part 2)

---
+++ ziplist.js
@@ -0,0 +1,16 @@
+const array1 = ['a', 'b', 'c'];
+const array2 = [1, 2, 3];
+function zipList(arr1, arr2) {
+  const arrReturn = [];
+  for (let i = 0; i < arr1.length; i++) {
+    arrReturn.push(arr1[i]);
+    arrReturn.push(arr2[i]);
+  }
+  return arrReturn;
+}
+console.log(zipList(array1, array2));
+
+function zipListTheSimpleWay(arr1, arr2) {
+  return _.flatten(_.zip(arr1,arr2));
+}
+console.log(zipListTheSimpleWay(array1, array2));
